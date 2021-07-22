a1228f4e6fb75a86fcd3b26dc1d83a708ed4ff27
Johnson Pasanna <johnsonpasanna1997@gmail.com>
2021-07-23 01:49:21 +0530

    API with error handling

--- script.js
+++ script.js
@@ -1,26 +1,41 @@
-// console.log("it is working through script.js file as well ")
-// var userName = prompt("Please type your name?");
-// // console.log(userName);
-// alert(userName);
-
 
 // querySelectors
-var buttonTranslate = document.querySelector(".btn-translate");
 var inputText = document.querySelector('.text-input');
-var outputText = document.querySelector('.text-output')
+var buttonTranslate = document.querySelector(".btn-translate");
+var outputText = document.querySelector('.text-output');
 
+// how to call an API via Javascript
+var serverURL = "https://api.funtranslations.com/translate/minion.json";
 
+// error handling
+function errorHandling(errorMessage) {
+  console.log("error occured", errorMessage);
+  alert("API calls not working! Since the server is down")
+}
 
 // function
-function clickEventHandler() {
-  outputText.innerHTML = inputText.value
+function translationURL(text) {
+  return testingURL = serverURL + "? " + "text=" + text
 }
+
+function clickEventHandler() {
+  var textInput = inputText.value;
+  fetch(translationURL(textInput))
+    .then(function responseHandler(response) {
+      return response.json()
+    })
+    .then(function logJSON(json) {
+      var finalTranslation = json.contents.translated;
+      outputText.innerText = finalTranslation;
+    })
+    .catch(errorHandler)
+};
 
 
 // Eventlistner
 buttonTranslate.addEventListener('click', clickEventHandler)
 
 
-
 // Conclusion: querySelector select the first element from HTML
 // click event what it does is when click happens it triggers a funciton call. call back funciton
+
