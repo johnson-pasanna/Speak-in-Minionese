2437e8014cc8bb70e44287ccf628cafb0cc7029c
Johnson Pasanna <johnsonpasanna1997@gmail.com>
2021-07-22 17:02:38 +0530

    adding button

--- script.js
+++ script.js
@@ -1,8 +1,24 @@
-console.log("it is working through script.js file as well ")
+// alert and prompt for output and input
 
-var userName = prompt("Please type your name?");
+// console.log("it is working through script.js file as well ")
+// var userName = prompt("Please type your name?");
+// // console.log(userName);
+// alert(userName);
 
-// console.log(userName);
+// ex03: wiring button click
 
-alert(userName);
+// challenge
+// reference Button using querySelector()
+// add event listener to button
+// console "clicked" on click.
+// check with different events
 
+var buttonTranslate = document.querySelector(".btn-translate");
+
+buttonTranslate.addEventListener('dblclick', function clickEventHandler() {console.log('clicked')})
+
+console.log(buttonTranslate);
+
+
+// Conclusion: querySelector select the first element element
+// click event what it does when click happens it triggers a funciton call. call back funciton
